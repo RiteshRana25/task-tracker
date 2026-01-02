31b94ea161ad87c5498cf8b0e028a7cbe9a43380
RiteshRana25 <160099737+RiteshRana25@users.noreply.github.com>
2026-01-03 00:31:59 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
 
 const fetchTasks = async () => {
   try {
-    const res = await axios.get("http://localhost:5000/api/tasks");
+    const res = await axios.get("https://task-tracker-backend-flax.vercel.app/tasks");
     setTasks(res.data);
   } catch (error) {
     console.error(error);
