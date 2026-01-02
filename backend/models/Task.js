c57ce69acb9a198bf2da3b07a3d0a1495cc25e7e
RiteshRana25 <160099737+RiteshRana25@users.noreply.github.com>
2026-01-03 00:16:27 +0530

    Update Task.js

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -4,11 +4,20 @@ const taskSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     description: String,
-    priority: { type: String, enum: ["Low", "Medium", "High"], default: "Low" },
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
+      default: "Low"
+    },
     dueDate: { type: Date, required: true },
-    status: { type: String, enum: ["Pending", "Completed"], default: "Pending" }
+    status: {
+      type: String,
+      enum: ["Pending", "Completed"],
+      default: "Pending"
+    }
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Task", taskSchema);
+module.exports =
+  mongoose.models.Task || mongoose.model("Task", taskSchema);
