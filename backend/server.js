9ebba9718622ed61cae4d30e3378db4c7bfc5bdb
RiteshRana25 <160099737+RiteshRana25@users.noreply.github.com>
2026-01-03 00:08:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/tasks", taskRoutes);
-
+app.get("/", (req, res) => {
+  res.send("API is working!");
+});
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
